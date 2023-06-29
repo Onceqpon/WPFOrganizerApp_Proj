65f847f9b4c33249dea6eccfdc2329d89ee160ca
Onceqpon <dawid.pietruszka2002@gmial.com>
2023-06-29 20:01:26 +0200

    Dodanie do aplikacji Menu oraz funkcji wylogowania

--- WPFOrganizerApp/MainWindow.xaml.cs
+++ WPFOrganizerApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPFOrganizerApp.AppPage;
 
 namespace WPFOrganizerApp
 {
@@ -26,6 +28,24 @@ namespace WPFOrganizerApp
         {
             InitializeComponent();
             userAccount.Text = "Zalogowano jako: " + logInName;
+        }
+
+        private void noteClick(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new NotePage();
+        }
+
+        private void todoClick(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new TodoPage();
+        }
+
+        private void logoutClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Wylogowano poprawnie");
+            LoginPage logiPage = new LoginPage();
+            logiPage.Show();
+            Close();
         }
     }
 }
